096ca3af6c97efeef46b0be0eb3b36fad090446f
Vicente <mortizl@unicartagena.edu.co>
2020-03-11 19:38:11 -0500

    Edite  el config

--- server/config/config.js
+++ server/config/config.js
@@ -2,9 +2,9 @@
 process.env.PORT = process.env.PORT || 3000;
 
 
-process.env.Node_ENV = process.env.Node_ENV || 'dev';
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
-if (process.env.Node_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
   urlDB = 'mongodb+srv://vic_ortiz:1992@cluster0-cnnt0.mongodb.net/test?retryWrites=true&w=majority';
